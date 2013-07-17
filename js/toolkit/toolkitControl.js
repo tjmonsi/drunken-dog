bbc29e260d64aee3c29bedaf5df3e6a939ad773e
Toni-Jan Keith Monserrat <tjmonsi@gmail.com>
2013-07-17 12:50:49 +0800

    Added main_toolkit as the main_toolkit

---
+++ js/toolkit/toolkitControl.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var Toolkit_Control = function(parent_tag) {
+	this.parent = parent_tag;
+}
+
+Toolkit_Control.prototype = {
+	init: function)() {
+		
+	}
+}
---
+++ js/toolkit/toolkitUI.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var UI = function(parent_tag) {
+	this.parent = parent_tag;
+}
+
+UI.prototype = {
+	init: function)() {
+		
+	}
+}
