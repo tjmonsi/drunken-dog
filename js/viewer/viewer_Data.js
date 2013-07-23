07508e3f3f4d11fc7f076a1778cfa8a34095fe24
Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>
2013-07-23 17:55:02 +0800

    Added viewer.html for prototype (PRIORITY)

---
+++ js/viewer/viewer_Data.js
@@ -0,0 +1,19 @@
+"use strict";
+
+var viewer_Data = function(parent) {
+	this.parent = parent;
+	this.videoplayers = {};
+
+	this.scene_objects = {};
+
+	this.assets = {};
+
+	this.init();
+
+}
+
+viewer_Data.prototype = {
+	init: function() {
+
+	}
+}
